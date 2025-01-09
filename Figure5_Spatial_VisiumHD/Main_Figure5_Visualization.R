29ac2eba7d9374ea5d4b3cda75ca9b3b6f244806
Jihong Tang <njutangjihong@gmail.com>
2025-01-09 11:34:08 +0800

    Fig 5E

--- Figure5_Spatial_VisiumHD/Main_Figure5_Visualization.R
+++ Figure5_Spatial_VisiumHD/Main_Figure5_Visualization.R
@@ -94,4 +94,85 @@ draw(ht_nei, heatmap_legend_side = "right",
      annotation_legend_side = "right")
 dev.off()
 
+# Fig 5D Example patch visualization ----
+## Up panel: H&E images of the selected example patch
+## Bottom panel: cell type annotation of the selected example patch
+
 # Fig 5E 
+## dotplots for all ---- 
+res_cor_cut0.05 <- do.call(rbind, lapply(cellstates, get_scc_res, 0.05))
+# res_cor_cut0.02 <- do.call(rbind, lapply(cellstates, get_scc_res, 0.02))
+orderID <- c("n_MES", "n_G2M", "n_G1S", "n_AC", "n_NPC", "n_OC", "n_Lym", "n_Mye", "n_Vas", "n_Olig")
+plt_dot <- res_cor_cut0.05[!(res_cor_cut0.05$Cell_State %in% c("n_lowQ", "n_Unknown")), ]
+plt_dot$dotSize <- ifelse(plt_dot$PCC_pvalue < 0.1, -log10(plt_dot$PCC_pvalue), 1)
+library(tidyverse)
+plot_2.data <- plt_dot[which(plt_dot$dotSize > 1), ]
+plot_3.data <- plt_dot[which(plt_dot$dotSize == 1), ]
+coMu.plot <- ggplot() +
+  theme_classic()
+coMu.plot <- coMu.plot + geom_vline(xintercept = unique(plt_dot$Cutoff), linetype = 2, color = "grey", size = 0.3)
+coMu.plot <- coMu.plot + geom_hline(yintercept = unique(plt_dot$Cell_State), linetype = 2, color = "grey", size = 0.3)
+
+coMu.plot <- coMu.plot + geom_point(data = plt_dot, aes(y = Cell_State, x = Cutoff, size = dotSize, fill = PCC), shape = 21, color = "white") + ylab(NULL) + xlab(NULL) + ggtitle(NULL)
+coMu.plot
+coMu.plot <- coMu.plot + geom_point(data = plot_2.data, aes(y = Cell_State, x = Cutoff, size = dotSize, fill = PCC), shape = 21) + ylab(NULL) + xlab(NULL) + ggtitle(NULL)
+coMu.plot
+coMu.plot <- coMu.plot + theme(
+  panel.background = element_rect(fill = "transparent", color = "black"), plot.margin = unit(c(1, 4, 1, 1), "lines"), plot.title = element_text(size = 24, vjust = 0.5, hjust = 0.5, face = "bold.italic"),
+  text = element_text(size = 24, vjust = 1.4, hjust = 0.5, face = "bold"), legend.key.width = unit(1, "cm"), legend.key.height = unit(0.5, "cm"), legend.position = "bottom",
+  legend.text = element_text(size = 12, face = "plain"), axis.text.y = element_text(size = 16, vjust = 0.5, hjust = 1, face = "plain", color = "black"), legend.title = element_text(size = 12, vjust = 0.5, hjust = 0, face = "plain"),
+  axis.text.x = element_text(size = 16, angle = 45, vjust = 1, hjust = 1, face = "plain", color = "black"), axis.title.x = element_text(size = 20, vjust = 0, hjust = 0.5, face = "plain", color = "black"),
+  axis.title.y = element_text(size = 20, hjust = 0.5, vjust = 2, face = "plain", color = "black"),
+  strip.text = element_text(size = 18, face = "bold", vjust = 0.5, hjust = 0.5), strip.background = element_rect(colour = "black", fill = gg_color_hue(3))
+)
+coMu.plot
+coMu.plot <- coMu.plot + scale_shape_manual(
+  name = NULL, values = c(A_green = 15, B_red = 17, C_grey = 16),
+  labels = c(A_green = "Mutual exclusion", B_red = "Co-occurrence", C_grey = "Not significant"),
+  guide = guide_legend(override.aes = list(size = 4), nrow = 4), na.translate = F
+)
+coMu.plot <- coMu.plot + scale_size(name = "Fisher's test\n   p-value", breaks = c(1, 2, 4, 6, 8), labels = expression(0.01, 10^-2, 10^-4, 10^-6, 10^-8), guide = guide_legend(nrow = 2), range = c(3, 6))
+coMu.plot <- coMu.plot + scale_fill_gradientn(name = "        Odds ratio\n(log10 transformed)", colours = c("#276419", "#4d9221", "#7fbc41", "white", "#de77ae", "#c51b7d", "#8e0152"), limits = c(-1, 1), breaks = seq(-1, 1, 0.5))
+coMu.plot <- coMu.plot + scale_x_discrete(position = "bottom") + scale_y_discrete(limits = rev(orderID))
+coMu.plot
+plotAll <- rbind(ggplotGrob(coMu.plot), size = "first")
+plotAll
+ggsave(file = "./figures/Fig5E_cor_spatialDensity.pdf", plot = plotAll, bg = "white", width = 6, height = 12, units = "cm", dpi = 600)
+
+## mes-like plots ----
+res_bin_ave_IR %>%
+  filter(Cell_State == "n_MES") %>%
+  ggplot() +
+  theme_classic() +
+  geom_point(aes(x = nCount_Ave, y = bins), size = 2, alpha = 1, color = "#4daf4a") +
+  stat_smooth(aes(x = nCount_Ave, y = bins), method = "lm", color = "black", se = F) +
+  scale_y_continuous(limits = c(0, 1)) +
+  theme(
+    panel.background = element_rect(fill = "transparent", color = "transparent"), plot.margin = unit(c(2, 2, 2, 2), "lines"),
+    plot.title = element_text(size = 34, vjust = 0.5, hjust = 0.5, face = "bold.italic", color = "transparent"), text = element_text(size = 14, face = "bold"),
+    legend.key.width = unit(0.6, "cm"), legend.key.height = unit(0.6, "cm"), legend.position = "right", legend.text = element_text(size = 14, hjust = 0, face = "bold"),
+    axis.text.x = element_text(size = 12, face = "bold", color = "black"), axis.text.y = element_text(size = 12, face = "bold", color = "black"),
+    axis.title.x = element_text(size = 14, face = "plain", color = "black"), axis.title.y = element_text(size = 14, face = "plain", color = "black")
+  ) +
+  xlab("Binned patch cell states density") +
+  ylab("GD prediction score")
+ggsave("figures/Fig5E_density_GDscore_n_MES.pdf", width = 3.43, height = 3)
+
+## Lym-like plots ----
+res_bin_ave_IR %>%
+  filter(Cell_State == "n_Lym") %>%
+  ggplot() +
+  theme_classic() +
+  geom_point(aes(x = nCount_Ave, y = bins), size = 2, alpha = 1, color = "#d53f88") +
+  stat_smooth(aes(x = nCount_Ave, y = bins), method = "lm", color = "black", se = F) +
+  scale_y_continuous(limits = c(0, 1)) +
+  theme(
+    panel.background = element_rect(fill = "transparent", color = "transparent"), plot.margin = unit(c(2, 2, 2, 2), "lines"),
+    plot.title = element_text(size = 34, vjust = 0.5, hjust = 0.5, face = "bold.italic", color = "transparent"), text = element_text(size = 14, face = "bold"),
+    legend.key.width = unit(0.6, "cm"), legend.key.height = unit(0.6, "cm"), legend.position = "right", legend.text = element_text(size = 14, hjust = 0, face = "bold"),
+    axis.text.x = element_text(size = 12, face = "bold", color = "black"), axis.text.y = element_text(size = 12, face = "bold", color = "black"),
+    axis.title.x = element_text(size = 14, face = "plain", color = "black"), axis.title.y = element_text(size = 14, face = "plain", color = "black")
+  ) +
+  xlab("Binned patch cell states density") +
+  ylab("GD prediction score")
+ggsave("figures/Fig5E_density_GDscore_n_Lym.pdf", width = 3.43, height = 3)
